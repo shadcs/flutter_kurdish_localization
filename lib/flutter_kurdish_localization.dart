55d362bdaed09938e27fed6693793f62fcd663d0
shadcs <shad.designer@gmail.com>
2025-08-24 18:05:31 +0300

    Use relative exports in flutter_kurdish_localization.dart for pub compliance

--- lib/flutter_kurdish_localization.dart
+++ lib/flutter_kurdish_localization.dart
@@ -1,5 +1,5 @@
 library flutter_kurdish_localization;
 
-export 'package:flutter_kurdish_localization/kurdish_material_localization_delegate.dart';
-export 'package:flutter_kurdish_localization/kurdish_widget_localization_delegate.dart';
-export 'package:flutter_kurdish_localization/kurdish_cupertino_localization_delegate.dart';
+export 'kurdish_material_localization_delegate.dart';
+export 'kurdish_widget_localization_delegate.dart';
+export 'kurdish_cupertino_localization_delegate.dart';
